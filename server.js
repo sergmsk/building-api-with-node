5b665f6d34efb453740c814b60b6d46e50f838b6
KS <me@gmail.com>
2017-05-16 15:35:38 +0300

    add-any-routing-path

--- server.js
+++ server.js
@@ -7,6 +7,35 @@ var app = express();//наш веб-сервер
 app.get('/', function(req,res){
     res.send('Hello API');//ответ
 })
+
+var artists = [
+  {
+    id: 1,
+    name: 'Metallica'
+  },
+  {
+    id: 2,
+    name: 'Iron Maiden'
+  },
+  {
+    id: 3,
+    name: 'Deep Purple'
+  }
+];
+
+app.get('/artists', function(req,res){
+    res.send(artists)
+})
+
+app.get('/artists/:id', function(req,res){
+    //console.log(req.params);
+    //console.log(req.params.id);
+    var artist = artists.find(function (artist){
+        return artist.id === Number(req.params.id);
+    });
+    //ответ
+    res.send(req.params.id);
+})
 app.listen(3012, function(){
     console.log('API app started');
 })
